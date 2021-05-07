ac2bb1856b91ea7c0ce3c5acc9310d6c28060e23
khh <visualkhh@gmail.com>
2021-05-07 20:27:35 +0900

    suz

--- english/news/README.md
+++ english/news/README.md
@@ -1,3 +1,6 @@
 edu new
 ===
 visualkhh@gmail.com
+
+link
+ - 클럽하우스 외신뉴스 가볍게 때려넣기: https://docs.google.com/document/d/1Wd_GGKwb_iHoVf4lEUjUDCYK16A6eYzi8qhbANmbonc/edit
--- util/text-parser/parser.js
+++ util/text-parser/parser.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch');
 var qs = require('querystring');
 const fs = require('fs');
 // import utf8 from 'utf8';
-const dic = require('../../english/dictionary');
+// const dic = require('../../english/dictionary');
 const source = require('./source.json');
 const newDic = {};
 
@@ -15,10 +15,10 @@ function timer(ms) {
 async function papagoStart() {
     const descriptions = [];
     for (var i = 0; i < source.length; i += 1) {
+        var content = source[i];
         if(!content) {
             continue;
         }
-        var content = source[i];
         var description = await papagoTranslate(content);
         var content_audio = await papagoAudio(content)
         // var description_audio = await papagoAudio(description)
@@ -56,26 +56,19 @@ async function papagoStart() {
 async function papagoTranslate(desc) {
     desc = qs.escape(desc);
     const rs = await fetch("https://papago.naver.com/apis/n2mt/translate", {
+        "credentials": "include",
         "headers": {
-            "accept": "application/json",
-            "accept-language": "ko",
-            "authorization": "PPG de6416b3-21d7-4642-a2a3-0d9e458f10f6:p+UfLB/8uu5IhES1AySfqg==",
-            "cache-control": "no-cache",
-            "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:88.0) Gecko/20100101 Firefox/88.0",
+            "Accept": "application/json",
+            "Accept-Language": "ko",
+            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
             "device-type": "pc",
-            "pragma": "no-cache",
-            "sec-ch-ua": "\"Chromium\";v=\"88\", \"Google Chrome\";v=\"88\", \";Not A Brand\";v=\"99\"",
-            "sec-ch-ua-mobile": "?0",
-            "sec-fetch-dest": "empty",
-            "sec-fetch-mode": "cors",
-            "sec-fetch-site": "same-origin",
-            "timestamp": "1612933360691",
             "x-apigw-partnerid": "papago",
-            "cookie": "NNB=MMUTUFGDLONF6; NRTK=ag#30s_gr#2_ma#-2_si#2_en#0_sp#0; ASID=70d97a1c00000175c05a8c8b00005e56; nx_ssl=2; _gid=GA1.2.1311689521.1612847211; papago_skin_locale=ko; JSESSIONID=001DDC44ECB967DCF439B1012CF41363; _ga=GA1.2.594285439.1598922588; _ga_7VKFYR6RV1=GS1.1.1612933345.222.0.1612933345.60"
+            "Timestamp": "1620385743197",
+            "Authorization": "PPG 8580801c-3987-4344-bcaf-ab385b3bd994:4b/sTtaj6j3/id8AYyZ6oQ=="
         },
         "referrer": "https://papago.naver.com/",
-        "referrerPolicy": "origin",
-        "body": "deviceId=de6416b3-21d7-4642-a2a3-0d9e458f10f6&locale=ko&dict=true&dictDisplay=30&honorific=false&instant=false&paging=false&source=en&target=ko&text="+desc+"&authroization=PPG%20de6416b3-21d7-4642-a2a3-0d9e458f10f6%3Ap%2BUfLB%2F8uu5IhES1AySfqg%3D%3D&timestamp=1612933360691",
+        "body": "deviceId=8580801c-3987-4344-bcaf-ab385b3bd994&locale=ko&dict=true&dictDisplay=30&honorific=false&instant=false&paging=false&source=en&target=ko&text="+desc+"&authroization=PPG%208580801c-3987-4344-bcaf-ab385b3bd994%3A4b%2FsTtaj6j3%2Fid8AYyZ6oQ%3D%3D&timestamp=1620385743197",
         "method": "POST",
         "mode": "cors"
     });
@@ -87,27 +80,18 @@ async function papagoTranslate(desc) {
 async function papago(key) {
     // var reqQuery = {"source":"en","target":"ko","text":key,"locale":"ko"};
     // var reqQueryStr = qs.escape(JSON.stringify(reqQuery));
-
-    const rs = await fetch("https://papago.naver.com/apis/dictionary/search?source=en&target=ko&text=" + key + "&locale=ko", {
+    const rs = await fetch("https://papago.naver.com/apis/dictionary/search?source=en&target=ko&text="+key+"&locale=ko", {
+        "credentials": "include",
         "headers": {
-            "accept": "application/json",
-            "accept-language": "ko,en-US;q=0.9,en;q=0.8,ja;q=0.7,ru;q=0.6,ko-KR;q=0.5",
-            "authorization": "PPG de6416b3-21d7-4642-a2a3-0d9e458f10f6:6wixW5Lp/HB/pozu5lHtJg==",
-            "cache-control": "no-cache",
-            "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-            "pragma": "no-cache",
-            "sec-ch-ua": "\"Chromium\";v=\"88\", \"Google Chrome\";v=\"88\", \";Not A Brand\";v=\"99\"",
-            "sec-ch-ua-mobile": "?0",
-            "sec-fetch-dest": "empty",
-            "sec-fetch-mode": "cors",
-            "sec-fetch-site": "same-origin",
-            "timestamp": "1612919793648",
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:88.0) Gecko/20100101 Firefox/88.0",
+            "Accept": "application/json",
+            "Accept-Language": "ko-KR,ko;q=0.8,en-US;q=0.5,en;q=0.3",
+            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
             "x-apigw-partnerid": "papago",
-            "cookie": "NNB=MMUTUFGDLONF6; NRTK=ag#30s_gr#2_ma#-2_si#2_en#0_sp#0; ASID=70d97a1c00000175c05a8c8b00005e56; nx_ssl=2; _gid=GA1.2.1311689521.1612847211; JSESSIONID=DBABF9FCBC267C161B1CEA81CBA03FEB; _ga=GA1.2.594285439.1598922588; papago_skin_locale=ko; _ga_7VKFYR6RV1=GS1.1.1612919710.219.1.1612919770.60"
+            "Timestamp": "1620385778668",
+            "Authorization": "PPG 8580801c-3987-4344-bcaf-ab385b3bd994:EIe9HjezTRHASiM0VGVDag=="
         },
         "referrer": "https://papago.naver.com/",
-        "referrerPolicy": "origin",
-        "body": null,
         "method": "GET",
         "mode": "cors"
     });
@@ -188,7 +172,7 @@ async function papagoAudio(desc) {
 //데이터 가져오기.
 papagoStart();
 
-// papago('that’s');
+// papago('word');
 // papagoStartFromDic();
 
 
