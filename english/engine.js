48c22249d15307b0cfad20942d8c9e207b393f4d
김현하(visual) <118410962+jaranda-visual@users.noreply.github.com>
2024-11-30 22:43:30 +0900

    add interview

--- english/engine.js
+++ english/engine.js
@@ -1,18 +1,96 @@
-var audio = new Audio();
-function play(url) {
-    audio.pause();
-    audio.src = url;
-    audio.play();
+// var audio = new Audio();
+const synth = window.speechSynthesis;
+let voices = synth.getVoices();
+function play(data) {
+    const url = data.audio;
+    const desc = data.text;
+
+    const utterThis = new SpeechSynthesisUtterance(desc);
+    const voiceSelect = document.querySelector(".voice-select");
+    const selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
+    for (const voice of voices) {
+        if (voice.name === selectedOption) {
+            utterThis.voice = voice;
+        }
+    }
+    utterThis.pitch = document.querySelector('.pitch').value;
+    utterThis.rate = document.querySelector('.rate').value;
+    synth.cancel();
+    synth.speak(utterThis);
+    // console.log('play', url, desc);
+    // audio.pause();
+    // audio.src = url;
+    // audio.play();
 }
 
+function populateVoiceList() {
+    voices = synth.getVoices().sort(function (a, b) {
+        const aname = a.name.toUpperCase();
+        const bname = b.name.toUpperCase();
+
+        if (aname < bname) {
+            return -1;
+        } else if (aname == bname) {
+            return 0;
+        } else {
+            return +1;
+        }
+    });
+    const voiceSelect = document.querySelector(".voice-select");
+    const selectedIndex =
+        voiceSelect.selectedIndex < 0 ? 0 : voiceSelect.selectedIndex;
+    voiceSelect.innerHTML = "";
+
+    for (let i = 0; i < voices.length; i++) {
+        const option = document.createElement("option");
+        option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+        if (voices[i].default) {
+            option.textContent += " -- DEFAULT";
+        }
+
+        option.setAttribute("data-lang", voices[i].lang);
+        option.setAttribute("data-name", voices[i].name);
+        voiceSelect.appendChild(option);
+    }
+    voiceSelect.selectedIndex = selectedIndex;
+}
 
 // descriptions = [];
-window.addEventListener('DOMContentLoaded', function(){
+window.addEventListener('DOMContentLoaded', function() {
 
     const body = document.querySelector('body');
+    const voiceContainer = document.createElement('div');
+    voiceContainer.classList.add('voice-container');
+    const voiceSelect = document.createElement('select');
+    voiceSelect.classList.add('voice-select');
+    voiceContainer.appendChild(voiceSelect);
+    const rate = document.createElement('input')
+    rate.classList.add('rate');
+    rate.setAttribute('type', 'range');
+    rate.setAttribute('min', 0.5);
+    rate.setAttribute('max', 2);
+    rate.setAttribute('step', 0.1);
+    rate.setAttribute('value', 1);
+    voiceContainer.appendChild(rate);
+    const pitch = document.createElement('input')
+    pitch.classList.add('pitch');
+    pitch.setAttribute('type', 'range');
+    pitch.setAttribute('min', 0);
+    pitch.setAttribute('max', 2);
+    pitch.setAttribute('step', 0.1);
+    pitch.setAttribute('value', 1);
+    voiceContainer.appendChild(pitch);
+    voiceContainer.appendChild(pitch);
+    body.appendChild(voiceContainer);
+
+    populateVoiceList();
+    if (synth.onvoiceschanged !== undefined) {
+        synth.onvoiceschanged = populateVoiceList;
+    }
 
 
-    DESCRIPTIONS.forEach((data,idx,i) => {
+    DESCRIPTIONS.forEach((data, idx, i) => {
         const div = document.createElement("div");
         div.classList.add('sentence-container');
 
@@ -21,9 +99,9 @@ window.addEventListener('DOMContentLoaded', function(){
         enDiv.classList.add('first-language');
         // enDiv.classList.add('listen');
         enDiv.appendChild(document.createTextNode(data.en.desc));
-        if(data.en.audio) {
-            enDiv.addEventListener('click', evt => play(data.en.audio));
-        }
+        // if(data.en.audio) {
+        enDiv.addEventListener('click', evt => play({audio: data.en.audio, text: data.en.desc}));
+        // }
 
         const koDiv = document.createElement("div");
         koDiv.classList.add('sentence');
@@ -110,7 +188,7 @@ attention = function (oldValue, slider, idx, wordKeys, enDiv, data) {
 }
 
 
-findWords = function(content) {
+findWords = function (content) {
     content = content.replace(/[\{\}\[\]\/?.,;:|\)*~'!^\-_+<>@\#$%&\\\=\(\"]/gi, ' ');
     content = content.toLowerCase();
     let contents = content.split(" ");
@@ -133,60 +211,59 @@ findWords = function(content) {
     return find;
 }
 
-makeExplainContainer = function(idx, words) {
-        let containDiv = document.createElement("div");
-        containDiv.classList.add("explain-container");
-        containDiv.classList.add("hide");
-        let findKeys = Object.keys(words);
-        if (findKeys.length > 0) {
-            findKeys.forEach((fkit, sidx, a) => {
-                let f = words[fkit];
+makeExplainContainer = function (idx, words) {
+    let containDiv = document.createElement("div");
+    containDiv.classList.add("explain-container");
+    containDiv.classList.add("hide");
+    let findKeys = Object.keys(words);
+    if (findKeys.length > 0) {
+        findKeys.forEach((fkit, sidx, a) => {
+            let f = words[fkit];
 
-                let wordDiv = document.createElement("div");
-                wordDiv.className = "explain word";
-                wordDiv.textContent = fkit + (f.phonetic?(" [" + f.phonetic+ "]"):"");
-                wordDiv.id = 'explain_' + idx + '_' + sidx;
-                if (f.audio) {
-                    wordDiv.addEventListener('click', evt => play(f.audio));
+            let wordDiv = document.createElement("div");
+            wordDiv.className = "explain word";
+            wordDiv.textContent = fkit + (f.phonetic ? (" [" + f.phonetic + "]") : "");
+            wordDiv.id = 'explain_' + idx + '_' + sidx;
+            // if (f.audio) {
+            wordDiv.addEventListener('click', evt => play({audio: f.audio, text: fkit}));
+            // }
+            containDiv.appendChild(wordDiv);
+
+            let meanDiv = document.createElement("div");
+            meanDiv.classList.add("explain");
+            meanDiv.classList.add("mean-container");
+
+
+            for (let i = 0; f.means && i < f.means.length; i++) {
+                let mean = f.means[i];
+                let meanAtDiv = document.createElement("div");
+
+                if (mean.type) {
+                    let typeDiv = document.createElement("div");
+                    typeDiv.className = "type";
+                    typeDiv.textContent = "[" + mean.type + "]";
+                    meanAtDiv.appendChild(typeDiv);
                 }
-                containDiv.appendChild(wordDiv);
 
-                let meanDiv = document.createElement("div");
-                meanDiv.classList.add("explain");
-                meanDiv.classList.add("mean-container");
-
-
-                for (let i = 0; f.means && i < f.means.length; i++) {
-                    let mean = f.means[i];
-                    let meanAtDiv = document.createElement("div");
-
-                    if (mean.type) {
-                        let typeDiv = document.createElement("div");
-                        typeDiv.className = "type";
-                        typeDiv.textContent = "[" + mean.type + "]";
-                        meanAtDiv.appendChild(typeDiv);
-                    }
-
-                    for (let j = 0; mean.mean && j < mean.mean.length; j++) {
-                        let meanSubDiv = document.createElement("div");
-                        meanSubDiv.className = "mean";
-                        // meanSubDiv.textContent = (j+1)+') '+mean.mean[j];
-                        meanSubDiv.textContent = mean.mean[j];
-                        meanAtDiv.appendChild(meanSubDiv);
-                    }
-                    meanDiv.appendChild(meanAtDiv);
+                for (let j = 0; mean.mean && j < mean.mean.length; j++) {
+                    let meanSubDiv = document.createElement("div");
+                    meanSubDiv.className = "mean";
+                    // meanSubDiv.textContent = (j+1)+') '+mean.mean[j];
+                    meanSubDiv.textContent = mean.mean[j];
+                    meanAtDiv.appendChild(meanSubDiv);
                 }
-                containDiv.appendChild(meanDiv);
-            });
-        }
-        return containDiv;
+                meanDiv.appendChild(meanAtDiv);
+            }
+            containDiv.appendChild(meanDiv);
+        });
+    }
+    return containDiv;
 }
 
 
-
-settingToggleBtn = function() {
+settingToggleBtn = function () {
     let sentenceContainers = document.querySelectorAll('.sentence-container');
-    const list = ['🐳','🐵', '🐧', '🐱', '🦊', '🐔', '🦄'];
+    const list = ['🐳', '🐵', '🐧', '🐱', '🦊', '🐔', '🦄'];
 
     for (let i = 0; i < sentenceContainers.length; i++) {
         var sentenceContainer = sentenceContainers[i];
@@ -202,7 +279,7 @@ settingToggleBtn = function() {
                     div.addEventListener("click", event => {
                         // console.log(window.getComputedStyle(at).display, at.style.visibility,'---');
                         // if (at.getAttribute("style") && at.getAttribute("style").indexOf("display:block") >= 0 ){
-                        if (window.getComputedStyle(at).display === 'block' ){
+                        if (window.getComputedStyle(at).display === 'block') {
                             at.setAttribute("style", "display:none")
                         } else {
                             at.setAttribute("style", "display:block")
